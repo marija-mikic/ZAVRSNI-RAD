7d195dcd1954144c7c2efc6cef143988a220fdc1
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-02-22 18:43:33 +0100

    autorizacija na bazu

--- app/core/App.php
+++ app/core/App.php
@@ -1,42 +1,69 @@
 <?php
 
-class App 
+class App
 {
     public static function start()
     {
-        $ruta=Request::getRuta();
+        //echo '<pre>';
+        //print_r($_SERVER);
+        //echo '</pre>';
+        $ruta = Request::getRuta();
+        //echo $ruta;
 
-        $djelovi=explode('/',$ruta);
+        $djelovi = explode('/',$ruta);
+        
+        //echo '<pre>';
+        //print_r($djelovi);
+        //echo '</pre>';
 
         $klasa='';
-        if(isset($djelovi[1])|| $djelovi[1]===''){
+        if(!isset($djelovi[1]) || $djelovi[1]===''){
             $klasa='Index';
         }else{
             $klasa=ucfirst($djelovi[1]);
         }
         $klasa .= 'Controller';
+        //echo $klasa;
+
+        $metoda='';
+        if(!isset($djelovi[2]) || $djelovi[2]===''){
+            $metoda='index';
+        }else{
+            $metoda=$djelovi[2];
+        }
+
+        //echo $klasa . '->' . $metoda . '()';
+
+        if(class_exists($klasa) && method_exists($klasa,$metoda)){
+            // klasa i metoda postoje, instancirati klasu i pozvati metodu
+            $instanca = new $klasa();
+            $instanca->$metoda();
+        }else{
+            // metoda na klasi ne postoji, obavijestiti korisnika
+            $view = new View();
+            $view->render('error404',[
+                'onoceganema' =>$klasa . '->' . $metoda
+            ]);
+            //echo $klasa . '->' . $metoda . '() ne postoji';
+        }
+
+        //$kontroler = new IndexController();
+        //$kontroler->index();
+
+    }
+
+    public static function config($kljuc)
+    {
+        $config = include BP_APP . 'konfiguracija.php';
+        return $config[$kljuc];
+    }
     
-    $metoda ='';
-    if(!isser($djelovi[2])|| $djelovi[2]===''){
-        $metoda='Index';
-    }else{
-        $metoda=$djelovi[2];
-    }
+    public static function autoriziran()
+    {
+        if(isset($_SESSION) && isset($_SESSION['autoriziran'])){
+            return true;
+        }
 
-    if (class_exists($klasa) && method_exists($klasa,$metoda)){
-        $instanca=new $klasa();
-        $instanca->$metoda ();
-    }else {
-        $view = new View();
-        $view->render ('eror404',[
-            'onoceganema' =>$klasa . '->' . $metoda
-        ]);
+        return false;
     }
-} 
-
-public static function config($kljuc)
-{
-    $config= include BP_APP . 'konfiguracija.php';
-    return $config[$kljuc];
-}
 }
