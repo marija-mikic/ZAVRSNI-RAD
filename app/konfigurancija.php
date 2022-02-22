1f7c511ce09c6f63016b2838a1f27eab3407594d
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-02-22 18:01:27 +0100

    unos spajanja na bazu

--- app/konfigurancija.php
+++ app/konfigurancija.php
@@ -3,5 +3,11 @@
 return [
     'dev'=> true,
     'url'=>'http//edunovaapp.xyz/',
-    'naslovApp'=>'PIZZERIA'
+    'naslovApp'=>'PIZZERIA',
+    'baza'=>[
+        'server'=>'localhost',
+        'baza'=>'zavrsni',
+        'korisnik'=>'edunova',
+        'lozinka'=>'edunova'
+    ]
 ];
