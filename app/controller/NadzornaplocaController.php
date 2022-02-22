db985d3dfbe10f41b84ab53638939c2f978acc7d
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-02-22 18:43:02 +0100

    zaštita privatnog dijela

---
+++ app/controller/NadzornaplocaController.php
@@ -0,0 +1,11 @@
+<?php
+
+class NadzornaplocaController extends AutorizacijaController
+{
+    private $viewDir = 'privatno' . DIRECTORY_SEPARATOR;
+
+    public function index()
+    {
+        $this->view->render($this->viewDir . 'nadzornaPloca');
+    }
+}
