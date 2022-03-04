bc4a0e2c36676aa7b5c53a83eb60535a6de91419
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-03-05 00:30:13 +0100

    slaganje datoteka

--- app/konfiguracija.php
+++ app/konfiguracija.php
@@ -1,5 +1,15 @@
 <?php
 
+if($_SERVER['SERVER_ADDR']==='127.0.0.1'){
+    $url='http://edunovaapp.xyz/';
+    $dev=true;
+    $baza=[
+        'server'=>'localhost',
+        'baza'=>'edunovapp24',
+        'korisnik'=>'edunova',
+        'lozinka'=>'edunova'
+    ];
+}else{
     $url='https://mamik-dj.shop/';
     $dev=false;
     $baza=[
@@ -8,11 +18,11 @@
         'korisnik'=>'marija',
         'lozinka'=>'xC5VCU&Kt=:3}8Af'
     ];
-
+}
 
 return [
     'dev'=>$dev,
     'url'=>$url,
-    'naslovApp'=>'PIZZERIA',
+    'naslovApp'=>'pizzerija',
     'baza'=>$baza
 ];
