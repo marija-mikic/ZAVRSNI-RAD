411d2418bef739ac6cc242e488f1777e8922e7ce
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-03-17 17:43:44 +0100

    unos jela

--- app/controller/JeloController.php
+++ app/controller/JeloController.php
@@ -1,18 +1,35 @@
 <?php
 
-class JeloController extends AutorizacijaController
 
+class JeloController extends AutorizacijaController
 {
 
-    private $viewDir=
-        'privatno' . DIRECTORY_SEPARATOR . 
-            'jela'. DIRECTORY_SEPARATOR ;
+    private $viewDir = 
+                'privatno' . DIRECTORY_SEPARATOR . 
+                    'jela' . DIRECTORY_SEPARATOR;
+    private $nf;
+    private $poruka;
+    private $jelo;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->nf = new \NumberFormatter("hr-HR", \NumberFormatter::DECIMAL);
+        $this->nf->setPattern('#,##0.00 kn');
+        $this->jelo = new stdClass();
+        $this->jelo->naziv='';
+        $this->jelo->sastav='';
+        $this->jelo->cijena='';
+        
+    }
 
     public function index()
-
     {
-       
-       $this->view->render($this->viewDir . 'index');
-             
+        $jela = Jelo::read();
+        
+        $this->view->render($this->viewDir . 'index',[
+        'jela' => $jela,
+        'css'=>'<link rel="stylesheet" href="' . App::config('url') . 'public/css/jeloindex.css">'
+       ]);
+    }
     }   
-} 
