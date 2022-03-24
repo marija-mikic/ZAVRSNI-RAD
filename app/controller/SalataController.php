93eef1e7b6d387bd5475c09877a47c8eb70aff4c
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-03-24 19:44:44 +0100

    izrada menija

---
+++ app/controller/SalataController.php
@@ -0,0 +1,30 @@
+<?php
+
+class SalataController extends JeloController
+
+{
+
+    private $viewDir=
+        'privatno' . DIRECTORY_SEPARATOR . 
+            'salate'. DIRECTORY_SEPARATOR ;
+
+            public function __construct()
+            {
+                parent::__construct();
+                $this->salata = new stdClass();
+                $this->salata->naziv='';
+                $this->salata->sastav='';
+                $this->salata->cijena='';
+                
+            }
+        
+            public function index()
+            {
+                $salate = Salata::read();
+                
+                $this->view->render($this->viewDir . 'index',[
+                'salate' => $salate,
+                'css'=>'<link rel="stylesheet" href="' . App::config('url') . 'public/css/piceindex.css">'
+               ]);
+            }
+        }
