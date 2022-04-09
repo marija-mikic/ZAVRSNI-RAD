a4646fe92b7dd6fb172d34a265485254148825d5
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-04-09 23:30:16 +0200

    brisanje number formater

--- app/controller/JeloController.php
+++ app/controller/JeloController.php
@@ -15,7 +15,6 @@ class JeloController extends Controller
     {
         parent::__construct();
         $this->jelo = new stdClass();
-        $this->nf = new \NumberFormatter("hr-HR", \NumberFormatter::DECIMAL);
         $this->nf->setPattern('#,##0.00 kn');
         $this->jelo->naziv='';
         $this->jelo->sastav='';
