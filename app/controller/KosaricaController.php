20be94ad2adea55799001ec2df13288831add979
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-04-14 19:05:03 +0200

    promjene košarice

--- app/controller/KosaricaController.php
+++ app/controller/KosaricaController.php
@@ -8,65 +8,42 @@ class KosaricaController extends AutorizacijaController
 
     public function index()
     {
-        $this->view->render($this->viewDir . 'index');
+    $kosarica= Kosarica::dohvatiNarudzbu($_SESSION['autoriziran']->id);
+        foreach($kosarica as $proizvod){
+            $proizvod->priceFormatted=$this->nf->format($proizvod->cijena);
+    {
+        $this->view->render($this->viewDir . 'index', [
+            'css' => $this->cssDir . 'index.css',
+            'narudzba' =>$narudzba,
+            'javascript'=>'<script src="'. App::config('url'). 'public/js/custom/removeFromCart.js"></script> '
+        ]);
     }
-
-    
-     
-
-
-
-   /* public function dodajNovi(){		
+    }
+    }
       
-        
-            Narudzba::create((array)$this->smjer);
-            //$this->index();
-            header('location:' . App::config('url').'smjer/index');
-        else{
-            $this->view->render($this->viewDir.'novi',[
-                'poruka'=>$this->poruka,
-                'smjer'=>$this->smjer
-            ]);
-	}
-    */
-
-    public function add(){
         //u bazu na narudžbu dodati atribut zavrseno boolean
         // vidjeti u bazu postoji li narudzba na kupcu koja ima završeno false
         //ako ima artikl dodati na taj id narudzbe 
         //ako nema prvo kreirati novu narudzbu na kupca s završeno false i na tu narudžbu dodati artikl
         //
 
-        if(isset($_POST["add"])){
-            if(isset($_SESSION["cart"])){
-                $item_array_id = array_column($_SESSION["cart"], "sifra");
-                if(!in_array($_GET["sifra"], $item_array_id)){
-                    $count = count($_SESSION["cart"]);
-                    
-                    $item_array = array(
-                        'sifra' => $_GET["sifra"],
-                        'naziv' => $_POST["naziv"],
-                        'cijena' => $_POST["cijena"],
-                        'kolicina' => $_POST["kolicina"],
-                        
-                    );
-                    $_SESSION["cart"][$count] = $item_array;
-                    echo '<script>window.location="index"</script>';
-                } else {					
-                    echo '<script>window.location="index"</script>';
-                }
-            } else {
-                $item_array = array(
-                    'sifra' => $_GET["sifra"],
-                    'naziv' => $_POST["naziv"],
-                    'cijena' => $_POST["cijena"],
-                    'kolicina' => $_POST["kolicina"]
-                );
-                $_SESSION["cart"][0] = $item_array;
-            }
+    public function dodaj($proizvodId, $kolicina=1)
+    {
+        $proizvodId = $_SESSION['autoriziran']->id;
+        if (Kosarica::viewNarudzba($kupacId) == null) {
+            Kosarica::kreiraj($kupacId);
         }
+        $narudzbaId = Kosarica::viewNarudzba($kupacId)->id;
     }
-    
+
+    public function izbaciizKosarice($proizvodId)
+    {
+        $kupacId = $_SESSION['autoriziran']->id;
+        $narudzbaId = Kosarica::viewNarudzba($kupacId)->id;
+    }
+
+
+        
     
 
 }
--- app/model/Kosarica.php
+++ app/model/Kosarica.php
@@ -1,5 +1,5 @@
 <?php
-class Kosarica{
+class Kosarica {
 
 	public static function read()
     {
@@ -12,41 +12,159 @@ class Kosarica{
         $izraz->execute();
         return $izraz->fetchAll();
     }
-}
 
+	public static function viewNarudzba($id) // provjera dali kupac ima već kreiranu započetu narudzbu   
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
 
-if(isset($_POST["add"])){
-	if(isset($_SESSION["cart"])){
-		$item_array_id = array_column($_SESSION["cart"], "sifra");
-		if(!in_array($_GET["sifra"], $item_array_id)){
-			$count = count($_SESSION["cart"]);
-			$item_array = array(
-				'sifra' => $_GET["sifra"],
-				'naziv' => $_POST["naziv"],
-				'cijena' => $_POST["cijena"],				
-				'ukupno' => $_POST["ukupno"]
-			);
-			$_SESSION["cart"][$count] = $item_array;
-			echo '<script>window.location="Kosarica.php"</script>';
-		} else {					
-			echo '<script>window.location="Kosarica.php"</script>';
-		}
-	} else {
-		$item_array = array(
-			'sifra' => $_GET["sifra"],
-			'naziv' => $_POST["naziv"],
-			'cijena' => $_POST["cijena"],
-			'ukupno' => $_POST["ukupno"],
-			
-		);
-		$_SESSION["cart"][0] = $item_array;
+		select sifra, kupac 
+		from narudzba 
+		where naruceno = false;
+            
+        ');
+        $izraz->execute([
+            'kupacId' => $id
+        ]);
+
+        return $izraz->fetch();
+    }
+
+	public static function kreiraj($id) // u koliko nema, kreira se nova narudzba
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+            insert into narudzba (kupac,naruceno) values
+            (:kupacId,false)
+            
+        ');
+        $izraz->execute([
+            'kupacId' => $id
+        ]);
+
+    }
+
+	public static function dodaj($proizvod, $narudzbaId, $kolicina)
+    {
+        $veza = DB::getInstanca();
+
+        $izraz = $veza->prepare('
+
+            select a.kolicina 
+			from narudzba_jelo a
+			inner join narudzba b on a.narudzba = b.sifra 
+			where a.jelo=:jelo and b.sifra=:narudzbaId;
+            
+        ');
+        $izraz->execute([
+            'proizvod' => $proizvod,
+            'narudzbaId' => $narudzbaId
+        ]);
+
+    
+        $akopostoji = $izraz->fetchColumn();
+
+        if($akopostoji == 0){
+            $izraz = $veza->prepare('
+
+            insert into narudzba (narudzba, proizvod, cijena, kolicina, datum) values
+            (:narudzbaId, :proizvod, (select cijena from proizvod where id = :proizvod), 1, now())
+            
+            ');
+            return $izraz->execute([
+                'proizvod' => $proizvod,
+                'narudzbaId' => $narudzbaId
+            ]);
+        }else{
+            $izraz = $veza->prepare('
+
+            update narudzba a
+            inner join narudzba as b on a.narudzba=b.id
+            set a.quantity = a.quantity+1
+            where proizvod= :proizvod and b.id= :narudzbaId
+            
+            ');
+            return $izraz->execute([
+                'proizvod' => $proizvod,
+                'narudzbaId' => $narudzbaId
+            ]);
+        }
+   
+    }
+
+	public static function dohvatiNarudzbu($id)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+
+		select a.sifra ,b.cijena,b.kolicina,a.proizvodi,a.ukupno  
+		from narudzba a
+		inner join narudzba_jelo b on a.sifra = b.narudzba 
+		inner join jelo c on b.jelo = c.sifra 
+		where a.naruceno = false;
+            
+        ');
+        $izraz->execute([
+            'kupacId' => $id
+        ]);
+
+        return $izraz->fetchAll();
+    }
+
+	public static function ukloniizKosarice($proizvod, $narudzbaId)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+            delete from narudzba_jelo 
+            where proizvod = :proizvod and narudzba = :narudzbaId
+            
+        ');
+        return $izraz->execute([
+            'proizvod' => $proizvod,
+            'narudzbaId' => $narudzbaId
+        ]);
+
+       
+    }
+
+	public static function brojProizvoda($id)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+
+            select count(*) as number
+            from narudzba a
+            inner join narudzba_jelo b on a.id=b.narudzba 
+            where a.naruceno = false  and a.kupac = :kupacId
+            
+        ');
+        $izraz->execute([
+            'kupacId' => $id
+        ]);
+
+        return $izraz->fetchColumn();
 	}
-	if (isset($_GET['clear'])) {
-		$stmt = $conn->prepare('DELETE FROM cart');
-		$stmt->execute();
-		$_SESSION['showAlert'] = 'block';
-		$_SESSION['message'] = 'All Item removed from the cart!';
-		header('location:kosarica.php');
-	  }
+
+	public static function ukupno($id)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+
+
+            select sum(b.cijena*b.kolicina) as number
+            from narudzba a
+            inner join narudzba_jelo b on a.id=b.narudzba
+            where a.naruceno = false and a.kupac = :kupacId
+            
+        ');
+        $izraz->execute([
+            'kupacId' => $id
+        ]);
+
+        return $izraz->fetchColumn();
+    }
 }
+
+
+
 
