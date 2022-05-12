be78d19718fd08b8dee47088222dd3c9e4acb660
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-05-12 23:38:02 +0200

    promjena tablica

--- zavrsnirad.sql
+++ zavrsnirad.sql
@@ -13,18 +13,19 @@ create table operater (
     uloga varchar (10) not null
 );
 
-create table jelo (
+create table proizvodi (
     sifra int  primary key not null auto_increment,
-    slika varchar(100),
     naziv varchar (50),
     sastav varchar(50) not null,
-    cijena decimal(18,2),
+    slika varchar(100),
+    cijena decimal(18,2),   
     vrsta int
 );
 
 create table kupac(
     sifra int primary key not null auto_increment,
     ime varchar (30) not null,
+    telefon int not null,
     prezime varchar(30) not null,
     adresa varchar(40) not null,
     email varchar(50) not null,
@@ -42,32 +43,18 @@ create table narudzba(
     adresa varchar(100) not null,
     datum datetime,
     ukupno int not null,
+    kupac int,
     naruceno boolean,
-    kupac int
+    status_narudzbe int
+
 );
 
-create table pice(
+create table narudzba_proizvodi(
     sifra int primary key not null auto_increment,
-    slika varchar(100),
-    naziv varchar(30) not null,
-    cijena decimal(18,2)not null,
-    vrsta int
-);
-
-create table narudzba_jelo(
-    sifra int primary key not null auto_increment,
-    jelo int,
     narudzba int,
     cijena decimal(18,2) not null,
-    kolicina int not null
-);
-
-create table narudzba_pice (
-    sifra int primary key not null auto_increment,
-    pice int,
-    narudzba int,
-    cijena decimal(18,2) not null,
-    kolicina int not null
+    kolicina int not null,
+    proizvodi int 
 );
 
 create table status_narudzbe(
@@ -76,18 +63,13 @@ zaprimljeno boolean,
 u_izradi boolean,
 u_dostavi boolean,
 dostavljeno boolean
-
 );
 
-
-alter table jelo add foreign key (vrsta) references vrsta(sifra);
+alter table proizvodi add foreign key (vrsta) references vrsta(sifra);
 alter table narudzba add foreign key (kupac) references kupac(sifra);
-alter table pice add foreign key (vrsta) references vrsta(sifra);
-alter table narudzba_jelo add foreign key(jelo) references jelo(sifra);
-alter table narudzba_pice add foreign key(pice) references pice(sifra);
-alter table narudzba_jelo add foreign key(narudzba) references narudzba(sifra);
-alter table narudzba_pice add foreign key(narudzba) references narudzba(sifra);
-
+alter table narudzba_proizvodi add foreign key(proizvodi) references proizvodi(sifra);
+alter table narudzba_proizvodi add foreign key(narudzba) references narudzba(sifra);
+alter table narudzba add foreign key(status_narudzbe) references status_narudzbe (sifra);
 
 insert into operater(email,lozinka,ime,prezime,uloga) values
 # lozinka a
@@ -101,18 +83,7 @@ insert into vrsta(sifra,naziv) values
 (null,'plate'),
 (null,'pizza');
 
-insert into kupac(sifra, ime,prezime,adresa,email,lozinka) values
-(null,'Marija','Mikic','B.Adzije 26','m@gmail','1'),
-(null,'Ana','Jurić','B.Jelacica 30','m@gmail','2'),
-(null,'Bruno','Klaric','Stepinca 10','m@gmail','3');
-
-insert into narudzba (sifra,adresa,datum,ukupno,naruceno,kupac) values
-(null,'dostava','2021-12-05','100',true,1),
-(null,'dostava','2021-12-05','100',false,2),
-(null,'dostava','2021-12-05','100',false,2);
-
-
-insert into jelo(sifra,naziv,sastav,slika,cijena,vrsta) values
+insert into proizvodi(sifra,naziv,sastav,slika,cijena,vrsta) values
 (null,'Čevapi u somunu','lepinja, luk, ajvar','mjesano.jpg','30',3),
 (null,'Čevapi i pommes frites','lepinja, luk, pommes frites, ajvar','cevapisapomesom.jpg','30',3),
 (null,'Ražnjići',' lepinja, luk, ajvar, pommes frites','raznjici.jpg','30',3),
@@ -121,21 +92,17 @@ insert into jelo(sifra,naziv,sastav,slika,cijena,vrsta) values
 (null,'Đakovačka','sir, rajčica, kulen, slanina, luk, ajvar','pizza3.jpg','31',4),
 (null,'Zelena','sir, rajčica, kulen, šunka, gorgonzola, vrhnje','zelenasalata.webp','32',1),
 (null,'Rajčica','sir, rajčica, kulen, čili, ljuti feferoni, ajvar','rajcica.jpg','30',1),
-(null,'Šopska','sir, rajčica, kulen, čili, ljuti feferoni, ajvar','sopskasalata.jpeg','30',1);
+(null,'Šopska','sir, rajčica, kulen, čili, ljuti feferoni, ajvar','sopskasalata.jpeg','30',1),
+(null,'pann','hmelj','pan.jpg','15',2),
+(null,'grasevina','bijelo vino','grasevina.jpg','25',2),
+(null,'cocacola','coca-colla','coca-cola.jpg','10',2);
 
-insert into pice(sifra,naziv,slika,cijena,vrsta) values
-(null,'pann','pan.jpg','15',2),
-(null,'grasevina','grasevina.jpg','25',2),
-(null,'cocacola','coca-cola.jpg','10',2);
+insert into kupac(sifra, ime,prezime,adresa,email,lozinka) values
+(null,'Marija','Mikic','B.Adzije 26','m@gmail','1'),
+(null,'Ana','Jurić','B.Jelacica 30','m@gmail','2'),
+(null,'Bruno','Klaric','Stepinca 10','m@gmail','3');
 
-insert into narudzba_pice (sifra,pice,narudzba,cijena,kolicina) values
-(null,1,1,'20','2');
-
-
-insert into narudzba_jelo (sifra,jelo,narudzba,kolicina,cijena) values
-(null,1,2,'2',20),
-(null,1,3,'2',20),
-(null,1,2,'2',20);
-
+insert into status_narudzbe(sifra,zaprimljeno,u_izradi,u_dostavi, dostavljeno)values
+(null,'1','2','3','4');
 
 
