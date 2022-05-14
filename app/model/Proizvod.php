dc31d97f819a32c826652f90b38f7acdb082b25f
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-05-14 21:36:26 +0200

    dodavanje novog modela, promjena tablice

---
+++ app/model/Proizvod.php
@@ -0,0 +1,72 @@
+<?php
+
+class Proizvod
+{
+    public static function readOne($kljuc)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            select * from jelo where sifra=:parametar;
+        
+        ');
+        $izraz->execute(['parametar' => $kljuc]);
+        return $izraz->fetch();
+    }
+    // CRUD
+
+
+    //R - Read
+    public static function read()
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            select * from jelo
+        
+        ');
+        $izraz->execute();
+        return $izraz->fetchAll();
+    }
+
+    public static function create($parametri)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            insert into proizvod (naziv, sastav, cijena)
+            values (:naziv,:sastav,:cijena);
+        
+        ');
+        $izraz->execute($parametri);
+
+    }
+
+    public static function update($parametri)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            update jelo set 
+                naziv=:naziv,
+                sastav=:sastav,
+                cijena=:cijena,
+                where sifra=:sifra;
+        
+        ');
+        $izraz->execute($parametri);
+
+    }
+
+    public static function delete($sifra)
+    {
+        $veza = DB::getInstanca();
+        $izraz = $veza->prepare('
+        
+            delete from jelo where sifra=:sifra;
+        
+        ');
+        $izraz->execute(['sifra' => $sifra]);
+
+    }
+}
