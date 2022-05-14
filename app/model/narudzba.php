b406f57d327ce1d2cd5dd1c58498f3f062b7eec8
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-05-14 21:38:12 +0200

    promjena kupca u korisnika

--- app/model/narudzba.php
+++ app/model/narudzba.php
@@ -7,14 +7,14 @@ class Narudzba
         $veza = DB::getInstanca();
         $izraz = $veza->prepare('
         
-        select a.sifra,a.datum,a.ukupno,a.kupac,a.naruceno,a.adresa,CONCAT_WS("",b.cijena, c.cijena) as cijena, CONCAT_WS("",b.kolicina, c.kolicina) as kolicina,
-        CONCAT_WS("",e.sifra, d.sifra) as sifra, CONCAT_WS("",e.naziv, d.naziv) as naziv, CONCAT_WS(" ",f.ime,f.prezime) as kupac
+        select a.sifra,a.datum,a.ukupno,a.korisnik,a.naruceno,a.adresa,CONCAT_WS("",b.cijena, c.cijena) as cijena, CONCAT_WS("",b.kolicina, c.kolicina) as kolicina,
+        CONCAT_WS("",e.sifra, d.sifra) as sifra, CONCAT_WS("",e.naziv, d.naziv) as naziv, CONCAT_WS(" ",f.ime,f.prezime) as korisnik
         from narudzba a
         left join narudzba_jelo b on a.sifra = b.narudzba
         left join narudzba_pice c on a.sifra = c.narudzba
         left join jelo d on b.jelo = d.sifra
         left join pice e on c.pice = e.sifra
-        left join kupac f on a.kupac = f.sifra
+        left join korisnik f on a.korisnik = f.sifra
         where a.naruceno = 1;
         
         '); 
