b75986db90fce36875787e1564203ea0c21e3d9e
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-05-24 19:12:42 +0200

    promjena tablice

--- zavrsnirad.sql
+++ zavrsnirad.sql
@@ -4,105 +4,227 @@ use zavrsnirad;
 # C:\xampp\mysql\bin\mysql -uedunova -pedunova --default_character_set=utf8 < D:\EdunovaPP24\zavrsnirad.sql
 alter database zavrsnirad character set utf8mb4;
 
-create table operater (
-    sifra int not null primary key auto_increment,
-    email varchar(50) not null,
-    lozinka char (60) not null,
-    ime varchar (50) not null,
-    prezime varchar (50) not null,
-    uloga varchar (10) not null
-);
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `ordered` tinyint(1) DEFAULT NULL,
+  `user` int(11) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `total_price` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table proizvodi (
-    sifra int  primary key not null auto_increment,
-    naziv varchar (50),
-    sastav varchar(50) not null,
-    slika varchar(100),
-    cijena decimal(18,2),   
-    vrsta int
-);
+-- --------------------------------------------------------
 
-create table kupac(
-    sifra int primary key not null auto_increment,
-    ime varchar (30) not null,
-    telefon int not null,
-    prezime varchar(30) not null,
-    adresa varchar(40) not null,
-    email varchar(50) not null,
-    lozinka char (60) not null
-  
-);
+--
+-- Table structure for table `order_product`
+--
 
-create table vrsta(
-    sifra int primary key not null auto_increment,
-    naziv varchar (50)
-);
+CREATE TABLE `order_product` (
+  `id` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  `product` int(11) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table narudzba(
-    sifra int primary key not null auto_increment,
-    adresa varchar(100) not null,
-    datum datetime,
-    ukupno int not null,
-    kupac int,
-    naruceno boolean,
-    status_narudzbe int
+-- --------------------------------------------------------
 
-);
+--
+-- Table structure for table `order_status`
+--
 
-create table narudzba_proizvodi(
-    sifra int primary key not null auto_increment,
-    narudzba int,
-    cijena decimal(18,2) not null,
-    kolicina int not null,
-    proizvodi int 
-);
+CREATE TABLE `order_status` (
+  `id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-create table status_narudzbe(
-sifra int primary key not null auto_increment,
-zaprimljeno boolean,
-u_izradi boolean,
-u_dostavi boolean,
-dostavljeno boolean
-);
+--
+-- Dumping data for table `order_status`
+--
 
-alter table proizvodi add foreign key (vrsta) references vrsta(sifra);
-alter table narudzba add foreign key (kupac) references kupac(sifra);
-alter table narudzba_proizvodi add foreign key(proizvodi) references proizvodi(sifra);
-alter table narudzba_proizvodi add foreign key(narudzba) references narudzba(sifra);
-alter table narudzba add foreign key(status_narudzbe) references status_narudzbe (sifra);
+INSERT INTO `order_status` (`id`, `status`) VALUES
+(1, 'Received'),
+(2, 'Preparation'),
+(3, 'Delivered');
 
-insert into operater(email,lozinka,ime,prezime,uloga) values
-# lozinka a
-('admin@mamik-dj.shop','$2a$12$gcFbIND0389tUVhTMGkZYem.9rsMa733t9J9e9bZcVvZiG3PEvSla','Administrator','Mamik-dj','admin'),
-# lozinka o
-('oper@mamik-dj.shop','$2a$12$S6vnHiwtRDdoUW4zgxApvezBlodWj/tmTADdmKxrX28Y2FXHcoHOm','Operater','Mamik-dj','oper');
+-- --------------------------------------------------------
 
-insert into vrsta(sifra,naziv) values
-(null,'salate'),
-(null,'pica'),
-(null,'plate'),
-(null,'pizza');
+--
+-- Table structure for table `product`
+--
 
-insert into proizvodi(sifra,naziv,sastav,slika,cijena,vrsta) values
-(null,'Čevapi u somunu','lepinja, luk, ajvar','mjesano.jpg','30',3),
-(null,'Čevapi i pommes frites','lepinja, luk, pommes frites, ajvar','cevapisapomesom.jpg','30',3),
-(null,'Ražnjići',' lepinja, luk, ajvar, pommes frites','raznjici.jpg','30',3),
-(null,'Peperone	rajčica',' sir, šunka, gljive, češnjak,feferoni','pizza1.jpg','30',4),
-(null,'Pršuto','sir, rajčica, pršut, masline, gljive, slanina','pizza2.jpg','32',4),
-(null,'Đakovačka','sir, rajčica, kulen, slanina, luk, ajvar','pizza3.jpg','31',4),
-(null,'Zelena','sir, rajčica, kulen, šunka, gorgonzola, vrhnje','zelenasalata.webp','32',1),
-(null,'Rajčica','sir, rajčica, kulen, čili, ljuti feferoni, ajvar','rajcica.jpg','30',1),
-(null,'Šopska','sir, rajčica, kulen, čili, ljuti feferoni, ajvar','sopskasalata.jpeg','30',1),
-(null,'pann','hmelj','pan.jpg','15',2),
-(null,'grasevina','bijelo vino','grasevina.jpg','25',2),
-(null,'cocacola','coca-colla','coca-cola.jpg','10',2);
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-insert into kupac(sifra, ime,prezime,adresa,email,lozinka) values
-(null,'Marija','Mikic','B.Adzije 26','m@gmail','1'),
-(null,'Ana','Jurić','B.Jelacica 30','m@gmail','2'),
-(null,'Bruno','Klaric','Stepinca 10','m@gmail','3');
+--
+-- Dumping data for table `product`
+--
 
-insert into status_narudzbe(sifra,zaprimljeno,u_izradi,u_dostavi, dostavljeno)values
-(null,'1','2','3','4');
+INSERT INTO `product` (`id`, `image_path`, `name`, `description`, `price`, `type`) VALUES
+(4, NULL, 'Cola', 'Sugar free', 15, 3),
+(5, NULL, 'Capricoza', 'Sir, salama', 25, 1),
+(6, NULL, 'Plata', 'Kobasice, rostilj', 350, 2),
+(7, NULL, 'Šopska', 'Feta sir', 35, 4);
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `product_type`
+--
+
+CREATE TABLE `product_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product_type`
+--
+
+INSERT INTO `product_type` (`id`, `name`) VALUES
+(1, 'Pizza'),
+(2, 'Barbecue'),
+(3, 'Drink'),
+(4, 'Salad');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','customer') CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `surname`, `address`, `telephone`, `email`, `password`, `role`) VALUES
+(11, 'Branimir', 'Tester', 'adfd22', '212345', 'test@example.com', '$2y$10$qdCRn6Y7lb/I.80LGtaqTOFCWD3673b4FlivcjI8K2z6DJ0W.NuCe', 'admin'),
+(14, 'Ante', 'Antić', 'sdnadn', '2323', 'ante@eamil.com', '$2y$10$D7jodSAyZ4DD4jDdxbqfo.D.qrbu1KL6SQFHsCVH69lrzl.ODy2Wm', 'customer'),
+(15, 'test', 'test', 'testsetse', '1212313', 'test@test.co', '$2y$10$JpBXMgmvCzLwLZ1K7GQ55e8Vye4RBtHKWYLbgLqT93pEP9UPjejku', 'customer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `korisnik` (`user`),
+  ADD KEY `status` (`status`);
+
+--
+-- Indexes for table `order_product`
+--
+ALTER TABLE `order_product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `narudzba` (`order`),
+  ADD KEY `proizvod` (`product`);
+
+--
+-- Indexes for table `order_status`
+--
+ALTER TABLE `order_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `type` (`type`);
+
+--
+-- Indexes for table `product_type`
+--
+ALTER TABLE `product_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `order_product`
+--
+ALTER TABLE `order_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_status`
+--
+ALTER TABLE `order_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `product_type`
+--
+ALTER TABLE `product_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_ibfk_2` FOREIGN KEY (`status`) REFERENCES `order_status` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `order_product`
+--
+ALTER TABLE `order_product`
+  ADD CONSTRAINT `order_product_ibfk_1` FOREIGN KEY (`order`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_product_ibfk_2` FOREIGN KEY (`product`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`type`) REFERENCES `product_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
