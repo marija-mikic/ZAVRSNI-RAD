5dd5cc1a7f1da14f710e0b414770c1bf965a878e
marija-mikic <93914251+marija-mikic@users.noreply.github.com>
2022-05-24 18:25:40 +0200

    prebacivanje na engleski te ubacivanje namespacea

---
+++ app/Core/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Core;
+
+use PDO;
+
+class Database extends PDO
+{
+    private static $instance = null;
+
+    public function __construct($db)
+    {
+        $dsn = 'mysql:host=' . $db['server'] . ';dbname=' . $db['database'] . ';charset=utf8mb4';
+        parent::__construct($dsn, $db['user'], $db['password']);
+        $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new self(App::config('database'));
+        }
+        return self::$instance;
+    }
+
+}
